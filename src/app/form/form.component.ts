1ef9b144de187b759a21dcfcba6309744996ff34
PavelYakupov <pyakupov@broadinstitute.org>
2021-06-03 21:07:08 +0300

    controls

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -10,27 +10,48 @@ export class FormComponent {
 
   form: FormGroup = new FormGroup({
     account: new FormGroup({
-      email: new FormControl('Email'),
-      password: new FormControl('Password'),
+      email: new FormControl('Email', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('Password', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     }),
     profile: new FormGroup({
       name: new FormControl('Name'),
       phone: new FormControl('Phone'),
       city: new FormControl('City',)
     }),
+    company: new FormGroup({
+      name: new FormControl('Name'),
+      ownership: new FormControl('Ownership'),
+      inn: new FormControl('Taxpayer Identification Number (INN)'),
+      kpp: new FormControl('Tax Registration Reason Code (KPP)'),
+      okpo: new FormControl('All-Russian Classifier of Entrepreneurs and Organizations (OKPO)'),
+      date: new FormControl('Date')
+    }),
+    contacts: new FormGroup({
+      name: new FormControl('Name'),
+      job: new FormControl('Job Title'),
+      phone: new FormControl('Phone')
+    })
   });
 
+  getErrorMessage() {
+    if (this.form.get('account.email')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.form.get('account.email') ? 'Not a valid email' : '';
+  }
+
   submit() {
-    console.log('Form submitted: ', this.form.value)
+    if (this.form.valid) {
+      const formData = {...this.form.value};
+      console.log('Form data: ', formData);
+      // console.log(this.form.get('account').get('email').value);
+    }
   }
 }
-
-// email = new FormControl('', [Validators.required, Validators.email]);
-//
-// getErrorMessage() {
-//   if (this.email.hasError('required')) {
-//     return 'You must enter a value';
-//   }
-//
-//   return this.email.hasError('email') ? 'Not a valid email' : '';
-// }
